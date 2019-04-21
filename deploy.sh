c353dae83307d0f1ed30e7249fbcb7b43618ad0a
gefangshuai <gefangshuai@163.com>
2019-04-21 21:36:22 +0800

    coding

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
 git push --all && git push --tag
 cd vue-speedy
-npm publish
+npm publish --registry http://npm.kanasinfo.cn
