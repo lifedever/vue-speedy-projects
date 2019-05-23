4554cd50329b8ce701ddda9f503c11da9a2b4e58
gefangshuai <gefangshuai@163.com>
2019-05-23 21:32:04 +0800

    v2.0.120

---
+++ vue-speedy/src/pages/examples/editor/ExpEditor.vue
@@ -0,0 +1,18 @@
+<template>
+    <Editor v-model="value"></Editor>
+</template>
+
+<script>
+    export default {
+        name: "Editor",
+        data(){
+            return {
+                value: 'hello world'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
