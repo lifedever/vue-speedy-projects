5defc1929d4a58d39d1983c0a110569a7b312c49
gefangshuai <gefangshuai@163.com>
2019-06-15 14:25:05 +0800

    coding

--- vue-speedy/src/components/editor/editor.vue
+++ vue-speedy/src/components/editor/editor.vue
@@ -75,7 +75,12 @@
                 default() {
                     return []
                 }
-            }
+            },
+            uploadImgShowBase64: {      // 是否上传图片为base64
+                type: Boolean,
+                default: true
+            },
+            uploadImgServer: String     // 上传图片的服务地址，与base64不能并存
         },
         computed: {
             editorId() {
@@ -93,9 +98,14 @@
             this.editor.customConfig.zIndex = 1
             this.editor.customConfig.onchangeTimeout = this.changeInterval
             this.editor.customConfig.menus = this.menus.concat(this.extraMenus)
+            if (this.uploadImgShowBase64) {
+                this.editor.customConfig.uploadImgShowBase64 = true
+            } else if (this.uploadImgServer) {
+                this.editor.customConfig.uploadImgServer = this.uploadImgServer
+            }
 
             // create这个方法一定要在所有配置项之后调用
-            this.editor.create()
+            this.editor.create();
             // 如果本地有存储加载本地存储内容
             let html = localStorage.editorCache
             if (html) this.editor.txt.html(html)
