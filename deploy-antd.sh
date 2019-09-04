27dac634cb5fc029be6a1dcfc97bc843335570f6
gefangshuai <gefangshuai@163.com>
2019-09-04 13:11:36 +0800

    coding

--- deploy-antd.sh
+++ deploy-antd.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-cd vue-speedy
+cd vue-speedy-antd
 yarn version
 cd ..
 git add . && git commit -m 'coding'
 git checkout master && git merge develop && git checkout develop
 git push --all && git push --tag
-cd vue-speedy
+cd vue-speedy-antd
 npm publish --registry http://npm.kanasinfo.cn
