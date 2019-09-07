d573e703150ed06838eaceda5aa9c5c066a420b6
gefangshuai <gefangshuai@163.com>
2019-09-07 11:04:55 +0800

    v0.1.10

---
+++ vue-speedy-antd-demo/src/main.js
@@ -0,0 +1,76 @@
+import Vue from "vue";
+import Inject from '../conf/main-inject'
+import pages from './pages'
+import adminLayout from "./layout/admin-layout";
+import router from "./router";
+
+import homeMenu from './pages/home/menu'
+import systemMenu from './pages/system/menu'
+
+const routerInstance = router(pages)
+
+Vue.use(adminLayout, {
+    menus: [
+        homeMenu,
+        {
+            id: 'tenowork',
+            name: 'Tenowork',
+            icon: 'project',
+            url: 'https://app.tenowork.com',
+            iframe: true
+        },
+        {
+            id: 'vue-speedy-antd',
+            name: 'Ant.Design-Vue',
+            icon: 'ant-design',
+            url: 'https://vue.ant.design/docs/vue/introduce-cn/',
+            iframe: true
+        },
+        {
+            id: 'example',
+            name: '示例',
+            icon: 'appstore',
+            children: [
+                {
+                    id: 'exampleBase',
+                    name: '基础示例',
+                    url: '/example',
+                },
+                {
+                    id: 'exampleSplit',
+                    name: '分割容器',
+                    url: '/example/split',
+                },
+                {
+                    id: 'exampleTree',
+                    name: '树容器',
+                    url: '/example/tree',
+                },
+            ]
+        },
+        systemMenu
+    ],
+})
+
+Vue.use(Inject, {
+    config: {
+        title: 'Vue Speedy Ant Design',
+        shortTitle: 'VD',
+        router: routerInstance
+    },
+    pages: pages,
+    modules: [
+        {
+            path: 'test',
+            module: {
+                namespaced: true,
+                state: {
+                    name: 'VueSpeedyAntD'
+                },
+                getters: {
+                    nameGet: state => state.name
+                }
+            }
+        }
+    ]
+})
---
+++ vue-speedy-antd-demo/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = require('./conf')({
+    lessModifyVars: {
+
+    },
+    title: 'Vue Speedy Ant Design',
+    baseUrl: '/',
+    devServer: {
+        port: 6060,
+        proxy: {
+            '/api': {
+                target: "http://localhost:18080",
+                changeOrigin: true,
+                secure: false,
+                pathRewrite: {
+                    '^/api': '/api'
+                }
+            }
+        }
+    }
+})
