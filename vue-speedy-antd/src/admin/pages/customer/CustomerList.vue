0c61ae50b0bb688c5b93fa378889e146dc1ea95b
gefangshuai <gefangshuai@163.com>
2019-09-23 18:02:45 +0800

    v0.2.18

---
+++ vue-speedy-antd/src/admin/pages/customer/CustomerList.vue
@@ -0,0 +1,20 @@
+<template>
+    <table-container url="/api/customers"
+                     ref="containerRef"
+                     operation-width="240px"
+                     :pageable="true"
+                     :operation="false">
+        <s-table-column title="用户名" prop="loginName" width="200px"></s-table-column>
+        <s-table-column title="创建日期" prop="createdDate" width="200px"></s-table-column>
+    </table-container>
+</template>
+
+<script>
+    export default {
+        name: "CustomerList"
+    }
+</script>
+
+<style scoped>
+
+</style>
