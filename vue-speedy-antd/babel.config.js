3463f5b4ba60ffdb0079f34b245e239193ccf76c
gefangshuai <gefangshuai@163.com>
2019-09-23 11:42:20 +0800

    coding

--- vue-speedy-antd/babel.config.js
+++ vue-speedy-antd/babel.config.js
@@ -1,3 +1,4 @@
+const plugins = []
 if (process.env.NODE_ENV === 'production') {
     plugins.push('transform-remove-console')
 }
@@ -5,7 +6,5 @@ module.exports = {
     presets: [
         '@vue/app'
     ],
-    plugins: [
-
-    ]
+    plugins
 }
