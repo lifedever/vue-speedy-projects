78c4ed223eaf7b2b62c1a8e265554d28896bf14a
gefangshuai <gefangshuai@163.com>
2019-09-25 17:30:35 +0800

    coding

--- vue-speedy-antd/src/admin/pages/home/Home.vue
+++ vue-speedy-antd/src/admin/pages/home/Home.vue
@@ -1,7 +1,13 @@
 <template>
     <base-container>
         哈哈， 这是首页
-        <div></div>
+        <div style="width: 400px;">
+            <a-input class="s-input-with-button-right">
+                <a-button slot="addonAfter" type="primary">
+                    获取验证码
+                </a-button>
+            </a-input>
+        </div>
     </base-container>
 </template>
 
--- vue-speedy-antd/vue.config.js
+++ vue-speedy-antd/vue.config.js
@@ -13,7 +13,7 @@ module.exports = require('./conf')({
         port: 6060,
         proxy: {
             '/api': {
-                target: "http://localhost:18080",
+                target: "https://boss.uat.manyibar.com",
                 changeOrigin: true,
                 secure: false,
                 pathRewrite: {
