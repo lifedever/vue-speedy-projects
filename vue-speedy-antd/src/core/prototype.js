c6a12370ae83e7c4be02e417294c1a1bcac2d38a
gefangshuai <gefangshuai@163.com>
2019-09-26 11:26:46 +0800

    coding

--- vue-speedy-antd/src/core/prototype.js
+++ vue-speedy-antd/src/core/prototype.js
@@ -2,7 +2,7 @@ import {appendPath} from "../utils/common";
 
 export default {
     install(Vue, option) {
-        String.prototype.appendPath = (path) => {
+        String.prototype.appendPath = function (path) {
             return appendPath(this, path)
         }
     }
