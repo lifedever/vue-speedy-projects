e0359559bfd249604b915a4079294f7514a5ddbc
gefangshuai <gefangshuai@163.com>
2019-12-24 17:32:52 +0800

    coding

--- vue-speedy-antd/src/admin/pages/home/Home.vue
+++ vue-speedy-antd/src/admin/pages/home/Home.vue
@@ -11,23 +11,33 @@
         <div style="margin-top: 40px;">
             <a-button @click="openNewTab" type="primary">打开新Tab并加载数据</a-button>
         </div>
+
+        <a-select defaultValue="lucy" style="margin-top: 40px; width: 120px" @change="handleChange">
+            <a-select-option value="jack">Jack</a-select-option>
+            <a-select-option value="lucy">Lucy</a-select-option>
+            <a-select-option value="disabled" disabled>Disabled</a-select-option>
+            <a-select-option value="Yiminghe">yiminghe</a-select-option>
+        </a-select>
     </base-container>
 </template>
 
 <script>
-import Menu from "../../class/Menu";
+    import Menu from "../../class/Menu";
 
-export default {
-    name: 'Home',
-    methods: {
-        openNewTab(){
-            let id = new Date().getTime()
-            let menu = new Menu(id = id, name = "Tab_" + id, '/test/' + id)
-            menu.selecedKey = 'home'
-            this.openTab(menu)
+    export default {
+        name: 'Home',
+        methods: {
+            openNewTab() {
+                let id = new Date().getTime()
+                let menu = new Menu(id = id, name = "Tab_" + id, '/test/' + id)
+                menu.selecedKey = 'home'
+                this.openTab(menu)
+            },
+            handleChange(value) {
+                this.$message.success(value)
+            }
         }
     }
-}
 </script>
 
 <style scoped></style>
