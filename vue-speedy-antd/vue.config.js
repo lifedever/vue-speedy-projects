73659c93bf09ce5399ba592573a45eb6fc7a2caf
gefangshuai <gefangshuai@163.com>
2019-12-25 14:31:44 +0800

    coding

--- vue-speedy-antd/vue.config.js
+++ vue-speedy-antd/vue.config.js
@@ -1,5 +1,10 @@
 module.exports = require('./conf')({
     title: 'Vue Speedy Ant Design',
+    lessModifyVars: {   // 用于覆盖主题配色，所有变量如下：https://github.com/vueComponent/ant-design-vue/blob/master/components/style/themes/default.less
+        'primary-color': '#4b8af6',
+        'error-color': '#ea4435',
+        'success-color': '#29a262',
+    },
     admin: {
         name: 'admin',
         entry: 'src/admin/main.js',
