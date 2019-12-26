2d6048185e47198fe67afe89aeb3e518756d4426
gefangshuai <gefangshuai@163.com>
2019-12-26 15:12:27 +0800

    v0.3.46

---
+++ vue-speedy-antd/src/components/partial/dropdown/SDropdown.vue
@@ -0,0 +1,18 @@
+<template>
+    <a-menu>
+
+    </a-menu>
+</template>
+
+<script>
+    export default {
+        name: "SMenuTree",
+        props: {
+            data: Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
