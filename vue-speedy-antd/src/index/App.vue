f06679a48f1d625ae4fbf4ad2f3dfdf263e4c6a2
gefangshuai <gefangshuai@163.com>
2020-10-13 15:00:37 +0800

    coding

--- vue-speedy-antd/src/index/App.vue
+++ vue-speedy-antd/src/index/App.vue
@@ -1,16 +1,28 @@
 <template>
-    <div id="app">
-        <router-view></router-view>
-        <s-modal></s-modal>
-    </div>
+    <a-config-provider :locale="locale">
+        <div id="app">
+            <router-view></router-view>
+            <s-modal></s-modal>
+        </div>
+    </a-config-provider>
 </template>
 
 <script>
     import SModal from "../components/global/s-modal/SModal";
+    import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN';
+    import {ConfigProvider} from 'ant-design-vue'
 
     export default {
         name: 'app',
-        components: {SModal},
+        components: {
+            SModal,
+            [ConfigProvider.name]: ConfigProvider
+        },
+        data() {
+            return {
+                locale: zhCN,
+            }
+        }
 
     }
 </script>
